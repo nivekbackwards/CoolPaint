52511d46ab9b523f9a90713713073a5af64f9e10
Andrey <izotov100@gmail.com>
2012-11-14 22:08:55 -0500

    added chat

--- app.js
+++ app.js
@@ -1,35 +1,43 @@
+var express = require('express'),
+    http    = require('http'),
+    routes = require('./routes');
 
-/**
- * Module dependencies.
- */
+var app = module.exports = express();
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
-
-var app = express();
-
+// Configuration
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
+  app.set('view engine', 'ejs');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'your secret here' }));
   app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
+// Routes
 app.get('/', routes.index);
-app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+var server = http.createServer(app);
+
+// WebSockets/Socket.IO
+var io      = require('socket.io', {'log level': 0}).listen(server);
+var chatApp = require('./chat');
+
+io.sockets.on('connection', function (socket) {
+  chatApp.init(socket);
+});
+
+server.listen(3000, function(){
+  console.log("Express server listening on port %d in %s mode",
+              server.address().port, app.settings.env);
 });

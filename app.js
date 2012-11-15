7190f528d17ecf660966c67f1f4548cdcef2aabd
Andrey <izotov100@gmail.com>
2012-11-14 23:40:38 -0500

    changed port

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ io.sockets.on('connection', function (socket) {
   chatApp.init(socket);
 });
 
-server.listen(9639, function(){
+server.listen(9999, function(){
   console.log("Express server listening on port %d in %s mode",
               server.address().port, app.settings.env);
 });
--- app.js~
+++ app.js~
@@ -37,7 +37,7 @@ io.sockets.on('connection', function (socket) {
   chatApp.init(socket);
 });
 
-server.listen(3000, function(){
+server.listen(9639, function(){
   console.log("Express server listening on port %d in %s mode",
               server.address().port, app.settings.env);
 });

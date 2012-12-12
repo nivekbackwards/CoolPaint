f6c275bca0d1d6ec659cd643f619a2f71d94c4de
Andrey <izotov100@gmail.com>
2012-12-12 18:05:33 -0500

    added newObject socket handler in clientside

--- public/javascripts/coolpaint.js
+++ public/javascripts/coolpaint.js
@@ -176,6 +176,10 @@ define(['jquery', 'fabric', 'socketIO'], function($, fabric, socket){
       socket.on('loginReject', function(){
         console.log('username rejected');
       });
+
+      socket.on('newObject', function(data){
+        console.log('new object received! ' + JSON.stringify(data));
+      });
     };
 
 

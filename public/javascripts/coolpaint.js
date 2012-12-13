d37ed26eedf9e032c706f0f41ddc0c32093d4d86
jmhummel <jmhummel@gmail.com>
2012-12-13 15:06:00 -0500

    diff skeleton stuff

--- public/javascripts/coolpaint.js
+++ public/javascripts/coolpaint.js
@@ -1,6 +1,7 @@
 define(['jquery', 'fabric', 'socketIO', 'jscolor'], function($, fabric, socket, jscolor){
     var myName = null;
     var canvas;
+    var prevCanvasJSON;
 
     var pencilButton;
     var handButton;
@@ -255,30 +256,11 @@ define(['jquery', 'fabric', 'socketIO', 'jscolor'], function($, fabric, socket, 
 /*						MOUSE UP							*/
     function mouseUpAttach() {
      	canvas.observe('mouse:up', function(e) {
-     		//console.log("mouse:up observer");
-     		if (canvas.isDrawingMode) {
-     			var lastObj = canvas.getObjects()[canvas.getObjects().length - 1];
-        		//console.log(lastObj);
-        		var serializedObj = JSON.stringify(lastObj)
-        	
-        		//to communicate with the server
-        		socket.emit('newObject', {object: serializedObj});
-        		//console.log("canvas: " + JSON.stringify(canvas));
-        	
-        		//lastObj.remove();
-        		//canvas.loadFromJSON('{"objects":[' + serializedObj + ']}');
-        		//for (var i=0; i<canvas.getObjects().length; i++) {
-        		//	console.log(i + ": " + JSON.stringify(canvas.getObjects()[i]));
-        		//}
-        		//addFromJSON(serializedObj);
-        		//canvas.renderAll();
-        	} else {
-        		var lastObj = canvas.getActiveObject();
-        		var serializedObj = JSON.stringify(lastObj)
-        		//to communicate with the server
-        		socket.emit('newObject', {object: serializedObj});
-        		//console.log("canvas: " + JSON.stringify(canvas));
-        	}
+     		var currCanvasJSON = JSON.stringify(canvas);
+     		var diff = jsondiffpatch.diff(prevCanvasJSON, currCanvasJSON);
+     		console.log('canvasDiff=' + diff);
+     		socket.emit('canvasDiff', {object: diff});
+     		prevCanvasJSON = currCanvasJSON;
      	});
      };
 
@@ -309,16 +291,13 @@ define(['jquery', 'fabric', 'socketIO', 'jscolor'], function($, fabric, socket, 
         console.log('username rejected');
       });
 
-      socket.on('newObject', function(data){
-        console.log('new object received! ID=' + data.object.id + ' DATA=' );
-        console.log(data);
-        addFromJSON(data.object);
+      socket.on('canvasDiff', function(data){
+        console.log('new canvasDiff received=' + data.diff );
+        jsondiffpatch.patch(currCanvasJSON, data.diff);
+        prevCanvasJSON = currCanvasJSON;
+        canvas.loadFromJSON(currCanvasJSON);
       });
       
-      socket.on('newObjectID', function(data) {
-      	console.log("new objectID received=" + data.id);
-      	lastObj.id = data.id;
-      });
     };
 
 
